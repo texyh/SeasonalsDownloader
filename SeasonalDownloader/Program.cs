d163f5f2e7fc5ce9a1d38d9981d1efbaa71b2219
bolorundurowb <ogatimo@gmail.com>
2016-12-16 20:57:00 +0100

    Initial commit

---
+++ SeasonalDownloader/Program.cs
@@ -0,0 +1,23 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeasonalDownloader
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var baseURL = "http://o2tvseries.com/";
+            string[] seasonals = {"Suits", "The-Big-Bang-Theory-2", "The-Originals-1", "The-Vampire-Diaries-3"};
+            foreach (string seasonal in seasonals)
+            {
+                string seasonalURL = baseURL + seasonal;
+                IWebDriver webDriver = new ChromeDriver();
+                webDriver.Navigate().GoToUrl(seasonalURL);
+                //IWebElement webElement = webDriver.FindElement(By.Name("q"));
+                //
+                webDriver.Close();
+            }
+        }
+    }
+}
