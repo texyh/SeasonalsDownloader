510b40826459cd8883e223f6604cb2b633c47586
bolorundurowb <ogatimo@gmail.com>
2016-12-18 15:12:20 +0100

    feature: adds list of seasonals, the base URL, and some imperfect logic...for now

--- SeasonalDownloader/Program.cs
+++ SeasonalDownloader/Program.cs
@@ -1,5 +1,9 @@
-﻿using OpenQA.Selenium;
+﻿using System;
+using System.Data;
+using System.IO;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using System.Linq;
 
 namespace SeasonalDownloader
 {
@@ -7,17 +11,41 @@ namespace SeasonalDownloader
     {
         public static void Main(string[] args)
         {
+            string videoFolder = Environment.GetFolderPath(Environment.SpecialFolder.MyVideos);
             var baseURL = "http://o2tvseries.com/";
             string[] seasonals = {"Suits", "The-Big-Bang-Theory-2", "The-Originals-1", "The-Vampire-Diaries-3"};
+            IWebDriver webDriver = new ChromeDriver();
             foreach (string seasonal in seasonals)
             {
                 string seasonalURL = baseURL + seasonal;
-                IWebDriver webDriver = new ChromeDriver();
                 webDriver.Navigate().GoToUrl(seasonalURL);
-                //IWebElement webElement = webDriver.FindElement(By.Name("q"));
                 //
-                webDriver.Close();
+                string seasonalTitle = webDriver.FindElement(By.ClassName("active")).Text;
+                string seasonalFolder = Path.Combine(videoFolder, seasonalTitle);
+                //
+                var latestSeason = webDriver.FindElements(By.PartialLinkText("Season ")).FirstOrDefault();
+                latestSeason?.Click();
+                //
+                var episodeLinks =
+                    webDriver.FindElements(By.PartialLinkText("Episode ")).Select(x => x.GetAttribute("href"));
+                foreach (var episodeLink in episodeLinks)
+                {
+                    webDriver.Navigate().GoToUrl(episodeLink);
+                    var downloadLink = webDriver.FindElement(By.PartialLinkText(".mp4"));
+                    //
+                    string fileName = downloadLink.Text;
+                    string fullFilePath = Path.Combine(seasonalFolder, fileName);
+                    if (!File.Exists(fullFilePath))
+                    {
+                        downloadLink.Click();
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
             }
+            webDriver.Close();
         }
     }
 }
