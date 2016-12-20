c1f25da6ba12f24427fcdaf858b810493c8e8732
bolorundurowb <ogatimo@gmail.com>
2016-12-20 20:06:18 +0100

    feature: rearrange code for better stability

--- SeasonalDownloader/Program.cs
+++ SeasonalDownloader/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Data;
 using System.IO;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
@@ -14,9 +13,9 @@ namespace SeasonalDownloader
             string videoFolder = Environment.GetFolderPath(Environment.SpecialFolder.MyVideos);
             var baseURL = "http://o2tvseries.com/";
             string[] seasonals = {"Suits", "The-Big-Bang-Theory-2", "The-Originals-1", "The-Vampire-Diaries-3"};
-            IWebDriver webDriver = new ChromeDriver();
             foreach (string seasonal in seasonals)
             {
+                IWebDriver webDriver = new ChromeDriver();
                 string seasonalURL = baseURL + seasonal;
                 webDriver.Navigate().GoToUrl(seasonalURL);
                 //
@@ -24,29 +23,32 @@ namespace SeasonalDownloader
                 string seasonalFolder = Path.Combine(videoFolder, seasonalTitle);
                 //
                 var latestSeason = webDriver.FindElements(By.PartialLinkText("Season ")).FirstOrDefault();
-                latestSeason?.Click();
-                //
-                var episodeLinks =
-                    webDriver.FindElements(By.PartialLinkText("Episode ")).Select(x => x.GetAttribute("href"));
-                foreach (var episodeLink in episodeLinks)
+                if (latestSeason != null)
                 {
-                    webDriver.Navigate().GoToUrl(episodeLink);
-                    var downloadLink = webDriver.FindElement(By.PartialLinkText(".mp4"));
+                    latestSeason.Click();
                     //
-                    string fileName = downloadLink.Text;
-                    string fullFilePath = Path.Combine(seasonalFolder, fileName);
-                    if (!File.Exists(fullFilePath))
+                    var episodeLinks =
+                        webDriver.FindElements(By.PartialLinkText("Episode ")).Select(x => x.GetAttribute("href"));
+                    foreach (var episodeLink in episodeLinks)
                     {
-                        downloadLink.Click();
-						webDriver.Navigate().Back();
-                    }
-                    else
-                    {
-                        break;
+                        webDriver.Navigate().GoToUrl(episodeLink);
+                        var downloadLink = webDriver.FindElement(By.PartialLinkText(".mp4"));
+                        //
+                        string fileName = downloadLink.Text;
+                        string fullFilePath = Path.Combine(seasonalFolder, fileName);
+                        if (!File.Exists(fullFilePath))
+                        {
+                            downloadLink.Click();
+                            webDriver.Navigate().Back();
+                        }
+                        else
+                        {
+                            break;
+                        }
                     }
                 }
+                webDriver.Close();
             }
-			webDriver.Close();
 			Console.WriteLine("Done.");
         }
     }
